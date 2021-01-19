a2704911c90daad52172c742b0dfe0388a9227a9
WangGaoJie <gjwang@coruitech.com>
2021-01-19 10:57:21 +0800

    解决了客户端关闭后未正确清理数据的问题

--- netchat.js
+++ netchat.js
@@ -1,27 +1,31 @@
 const net = require('net');
 
-let clientArr = [];
+let clientSet = [];
 const server = net.createServer();
 
 server.on('connection', (person) => {
-    person.id = clientArr.length;
-    clientArr.push(person);
+    clientSet.push(person);
     person.setEncoding('utf8');
 
     person.on('data', (chunk) => {
-        console.log(chunk);
-        clientArr.forEach((val) => {
-            val.write(chunk);
+        clientSet.forEach((client) => {
+            client.write(chunk);
         })
     })
 
-    person.on('close', (person) => {
-        clientArr[person.id] = null;
+    person.on('close', (state) => {
+        clientSet = clientSet.filter((item) => {
+            return item != person;
+        })
     })
 
-    person.on('error', (person) => {
-        clientArr[person.id] = null;
+    person.on('error', (err) => {
+        clientSet = clientSet.filter((item) => {
+            return item != person;
+        })
+        person.destroy()
     })
 })
 
+console.log("Net Chat running...");
 server.listen(8000);
